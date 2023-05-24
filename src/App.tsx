e05769b99f9074a0be617e0d11c32820ddc465df
Michaell Alavedra <37460957+gitchaell@users.noreply.github.com>
2023-05-24 00:50:31 +0000

    retrying deploy

--- src/App.tsx
+++ src/App.tsx
@@ -4,9 +4,9 @@ import { AppProvider } from '@/App.context';
 import { GeolocationProvider } from '@/core/geolocation/presentation/context/Geolocation.provider';
 import { CurrencyProvider } from '@/core/currency/presentation/context/Currency.provider';
 import { WorkProvider } from './core/work/presentation/context/Work.provider';
+import { TaskProvider } from './core/task/presentation/context/Task.provider';
 
 import { Router } from '@/routes/Router';
-import { TaskProvider } from './core/task/presentation/context/Task.provider';
 
 export const App = () => {
 	return (
