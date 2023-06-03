5225179eedc4f4437722a683760c7cb8e1573a36
Michaell Alavedra <37460957+gitchaell@users.noreply.github.com>
2023-06-03 04:42:17 +0000

    refactor

--- src/core/task/presentation/context/Task.provider.tsx
+++ src/core/task/presentation/context/Task.provider.tsx
@@ -6,10 +6,12 @@ import { FindTasksDTO } from '@/core/task/domain/dtos/FindTasks.dto';
 import { TaskCreatedEvent, TaskCreatedEventListener } from '@/core/task/application/events/TaskCreated.event';
 import { TaskUpdatedEvent, TaskUpdatedEventListener } from '@/core/task/application/events/TaskUpdated.event';
 import { TaskDeletedEvent, TaskDeletedEventListener } from '@/core/task/application/events/TaskDeleted.event';
+import { UnloadEvent, UnloadEventListener } from '@/core/common/events/Unload.event';
 import { TaskTimedEvent } from '@/core/task/application/events/TaskTimed.event';
 import { TaskService } from '@/core/task/application/Task.service';
 import { TaskContext } from '@/core/task/presentation/context/Task.context';
 import { TaskState } from '@/core/task/presentation/state/Task.state';
+import { TaskMapper } from '@/core/task/infrastructure/Task.mapper';
 
 export const TaskProvider = ({ children }: { children: JSX.Element }) => {
 	const [{ taskSelected }, dispatch] = useReducer(
@@ -21,12 +23,16 @@ export const TaskProvider = ({ children }: { children: JSX.Element }) => {
 	const [tasks, setTasks] = useState([] as Task[]);
 
 	const findTasks = useCallback((params: FindTasksDTO) => {
-		const tasks = TaskService.findTasks(params);
+		const tasks = TaskService.findTasks(params).map(TaskMapper.toModel);
 		setTasks(tasks);
 		return tasks;
 	}, []);
 
-	const selectTask = useCallback((task: Task) => {
+	const selectTask = useCallback((task: TaskEntity | Task) => {
+		if (task instanceof Task) {
+			task = TaskMapper.toEntity(task);
+		}
+
 		dispatch({ type: 'task/selected', payload: task });
 	}, []);
 
@@ -34,43 +40,82 @@ export const TaskProvider = ({ children }: { children: JSX.Element }) => {
 		dispatch({ type: 'task/unselected', payload: null });
 	}, []);
 
-	const startTask = useCallback((task: Task) => {
-		return TaskService.startTask(task);
+	const startTask = useCallback((task: TaskEntity | Task) => {
+		if (task instanceof Task) {
+			task = TaskMapper.toEntity(task);
+		}
+
+		const taskStarted = TaskService.startTask(task);
+		const taskModel = TaskMapper.toModel(taskStarted);
+
+		return taskModel;
 	}, []);
 
-	const stopTask = useCallback((task: Task) => {
-		return TaskService.stopTask(task);
+	const stopTask = useCallback((task: TaskEntity | Task) => {
+		if (task instanceof Task) {
+			task = TaskMapper.toEntity(task);
+		}
+
+		const taskStopped = TaskService.stopTask(task);
+		const taskModel = TaskMapper.toModel(taskStopped);
+
+		return taskModel;
 	}, []);
 
-	const createTask = useCallback((task: TaskEntity) => {
+	const createTask = useCallback((task: TaskEntity | Task) => {
+		if (task instanceof Task) {
+			task = TaskMapper.toEntity(task);
+		}
+
 		const taskCreated = TaskService.createTask(task);
-		setTasks((previous) => [...previous, taskCreated]);
-		return taskCreated;
+		const taskModel = TaskMapper.toModel(taskCreated);
+
+		return taskModel;
 	}, []);
 
-	const updateTask = useCallback((task: TaskEntity) => {
+	const updateTask = useCallback((task: TaskEntity | Task) => {
+		if (task instanceof Task) {
+			task = TaskMapper.toEntity(task);
+		}
+
 		const taskUpdated = TaskService.updateTask(task);
-		setTasks((previous) => previous.map((task) => (task.id === taskUpdated.id ? taskUpdated : task)));
-		return taskUpdated;
+		const taskModel = TaskMapper.toModel(taskUpdated);
+
+		return taskModel;
 	}, []);
 
-	const deleteTask = useCallback((task: Task) => {
+	const deleteTask = useCallback((task: TaskEntity | Task) => {
+		if (task instanceof Task) {
+			task = TaskMapper.toEntity(task);
+		}
+
 		const taskDeleted = TaskService.deleteTask(task);
-		setTasks((previous) => previous.filter((task) => task.id !== taskDeleted.id));
-		return taskDeleted;
+		const taskModel = TaskMapper.toModel(taskDeleted);
+
+		return taskModel;
 	}, []);
 
 	useEffect(() => {
 		const handleTaskCreated: TaskCreatedEventListener = (event) => {
-			createTask(event.detail.task);
+			const taskModel = TaskMapper.toModel(event.detail.task);
+			setTasks((previous) => [...previous, taskModel]);
 		};
 
 		const handleTaskUpdated: TaskUpdatedEventListener = (event) => {
-			updateTask(event.detail.task);
+			const taskModel = TaskMapper.toModel(event.detail.task);
+			setTasks((previous) =>
+				previous.map((task) => (task.id.get() === taskModel.id.get() ? taskModel : task))
+			);
 		};
 
 		const handleTaskDeleted: TaskDeletedEventListener = (event) => {
-			deleteTask(event.detail.task);
+			const taskModel = TaskMapper.toModel(event.detail.task);
+			setTasks((previous) => previous.filter((task) => task.id.get() !== taskModel.id.get()));
+		};
+
+		const handleUnload: UnloadEventListener = (event) => {
+			event.preventDefault();
+			TaskService.stopAllTasks();
 		};
 
 		TaskCreatedEvent.subscribe(handleTaskCreated);
@@ -79,12 +124,16 @@ export const TaskProvider = ({ children }: { children: JSX.Element }) => {
 
 		TaskTimedEvent.subscribe(handleTaskUpdated);
 
+		UnloadEvent.subscribe(handleUnload);
+
 		return () => {
 			TaskCreatedEvent.unsubscribe(handleTaskCreated);
 			TaskUpdatedEvent.unsubscribe(handleTaskUpdated);
 			TaskDeletedEvent.unsubscribe(handleTaskDeleted);
 
 			TaskTimedEvent.unsubscribe(handleTaskUpdated);
+
+			UnloadEvent.unsubscribe(handleUnload);
 		};
 	}, []);
 
@@ -92,7 +141,7 @@ export const TaskProvider = ({ children }: { children: JSX.Element }) => {
 		<TaskContext.Provider
 			value={{
 				tasks,
-				taskSelected,
+				taskSelected: taskSelected ? TaskMapper.toModel(taskSelected) : null,
 				selectTask,
 				unselectTask,
 				startTask,
